f8255e72954815f2e568362c6daac7bdf052bf26
mting314 <57047134+mting314@users.noreply.github.com>
2021-05-02 01:45:31 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+# from flask_session import Session
+from werkzeug.utils import secure_filename
+import pandas as pd
+from io import StringIO
+from dotenv import load_dotenv
+
+from calculations import calculate_merge_rate
+
+
+load_dotenv();
+
+app = Flask(__name__)
+
+# ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# @app.route('/')
+# def index():
+#     return f'<h2>Hi' \
+#            f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+#            f'<a href="/playlists">my playlists</a> | ' \
+#            f'<a href="/currently_playing">currently playing</a> | ' \
+# 		   f'<a href="/current_user">me</a>' \
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            s=str(file.read(),'utf-8')
+
+            data = StringIO(s) 
+
+            df=pd.read_csv(data)
+
+            # guess cityname
+            cityname = filename[:filename.find("_Dispatch")]
+
+
+            merge_rate = calculate_merge_rate(df, cityname=cityname)
+            return render_template("base.html", merge_rate=merge_rate)
+    return render_template("base.html")
+
+
+'''
+Following lines allow application to be run more conveniently with
+`python app.py` (Make sure you're using python3)
+(Also includes directive to leverage pythons threading capacity.)
+'''
+if __name__ == '__main__':
+    # app.run(threaded=True,debug=True, port=int(os.environ.get("PORT",
+    #                                                os.environ.get("SPOTIPY_REDIRECT_URI", "8080").split(":")[-1])))
+    app.run(threaded=True,debug=True, port=int(os.environ.get("PORT",
+                                                os.environ.get("SPOTIPY_REDIRECT_URI", "8080").split(":")[-1])))
---
+++ calculations.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import ntpath
+
+from constants import list_off, time_name, dispatch_name, prio_name, desc_name, dispo_name, call_name
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+def determine_officer_initiated(df, cityname):
+    officer_initiated = 0
+    useless = {"assist","warrant","chase","foot","pursuit","traffic","ETS","routine","executive","parking","walk-in","fire","mark-out","driving","truant"}
+    # useless.extend(list_off[city])
+    
+    description_col = desc_name[cityname]
+    
+    if "N/A" not in description_col:
+        description = df[description_col]
+        for word in useless | set(list_off[cityname]):
+            if (all(item not in description.lower() for item in ["fired", "gunfire", "firework"])
+                and word in description.lower()):
+                    officer_initiated = 1
+            
+    if "N/A" not in dispatch_name[cityname] and any(item in cityname for item in ["Detroit","Los Angeles","Mesa","Portland","Sandy","Springs"]):
+        if time_name[cityname] == dispatch_name[cityname]:
+            officer_initiated = 1 
+
+    if "Elgin" in cityname:
+        if "F" in prio_name[cityname]:
+            officer_initiated = 1 
+
+    if "Harlingen" in cityname:
+        officer_initiated = 0
+        if df["callsource"].lower() == "officer initiated":
+            officer_initiated = 1 
+    
+    if "Richmond" in cityname:
+        if df["isselfinitiated"] == "No": 
+            officer_initiated = 0 
+        if df["isselfinititated"] == "No": 
+            officer_initiated = 0
+        if df["isselfinitiated"] == "Yes":
+            officer_initiated = 1
+        if df["isselfinititated"] == "Yes": 
+            officer_initiated = 1 
+
+    if "Norwalk" in cityname:
+        officer_initiated = 0
+        if "officer-initiated" in dispo_name["cityname"]:
+            officer_initiated = 1
+
+    if "Reno" in cityname or "Sparks" in cityname:
+        if description.lower() == "t":
+            officer_initiated = 1
+    
+    if "San Mateo" in cityname: 
+        officer_initiated = 1 if df["sourceofcall"] == "Officer Initiated" else 0
+    
+    if "Tacoma" in cityname:
+        officer_initiated = 1 if df["on_view"] == 1 else 0
+
+    return officer_initiated
+
+def calculate_merge_rate(df, cityname):
+
+    # read in demographic data
+    demo_data = pd.read_stata("demographic_data.dta")["fips_tract_10"]
+
+    # preprocessing
+    df["FIPS"] = pd.to_numeric(df["FIPS"], errors='coerce')
+    df.columns = [x.lower().replace(" ", "_") for x in df.columns]
+
+    df["officer_initiated"] = df.apply(determine_officer_initiated, cityname=cityname, axis=1)
+    
+    test_data = df.loc[df["officer_initiated"] == 0]
+    
+    total_overall = len(test_data)
+
+
+
+    return len(test_data.merge(demo_data, left_on="fips", right_on="fips_tract_10"))/total_overall
+
+if __name__ == "__main__":
+    from tkinter import Tk     # from tkinter import Tk for Python 3.x
+    from tkinter.filedialog import askopenfilename
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filepath = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+
+    filename = path_leaf(filepath)
+    cityname = filename[:filename.find("_Dispatch")]
+
+    df = pd.read_csv(filepath)
+    df["FIPS"] = pd.to_numeric(df["FIPS"], errors='coerce')
+    print(calculate_merge_rate(df, cityname=cityname))
